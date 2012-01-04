9f0a5c53af0660dcf0b60c263bda0308dfc63949
Alex Yakavenka <ayakav@codeaurora.org>
2012-01-04 06:38:58 -0800

    TelephonyProvider: Add support for nai
    Populate database with nai from apns-conf.xml
    
    Change-Id: I0d28f05ce1440814ab52a8a090bb91d8103df61d
    CRs-Fixed: 477428

--- src/com/android/providers/telephony/TelephonyProvider.java
+++ src/com/android/providers/telephony/TelephonyProvider.java
@@ -145,6 +145,7 @@ public class TelephonyProvider extends ContentProvider
                     "type TEXT," +
                     "current INTEGER," +
                     "protocol TEXT," +
+                    "profile_type TEXT," +
                     "roaming_protocol TEXT," +
                     "carrier_enabled BOOLEAN," +
                     "bearer INTEGER);");
@@ -241,12 +242,19 @@ public class TelephonyProvider extends ContentProvider
          * @return the row or null if it's not an apn
          */
         private ContentValues getRow(XmlPullParser parser) {
-            if (!"apn".equals(parser.getName())) {
+            ContentValues map = new ContentValues();
+            // get the profile type from the XML file tags
+            String prof_type = parser.getName();
+            if (prof_type == null) {
                 return null;
             }
-
-            ContentValues map = new ContentValues();
-
+            if (prof_type.equals("apn")) {
+                map.put(Telephony.Carriers.PROFILE_TYPE, "apn");
+            } else if (prof_type.equals("nai")) {
+                map.put(Telephony.Carriers.PROFILE_TYPE, "nai");
+            } else {
+                return null;
+            }
             String mcc = parser.getAttributeValue(null, "mcc");
             String mnc = parser.getAttributeValue(null, "mnc");
             String numeric = mcc + mnc;
